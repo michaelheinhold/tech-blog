2b940b9645f677b26d0a541b40c93a83e84f4db9
DerVerruckte <michael.heinhold@gmail.com>
2021-10-25 12:58:01 -0600

    post routes

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+const { Post, User } = require('../../models');
+
+router.get('/', (req, res) => {
+  Post.findAll({
+    attributes: [
+      'id',
+      'title',
+      'content',
+      'user_id',
+      'created_at',
+    ],
+    include: {
+      model: User,
+      attributes: ['username']
+    }
+  })
+    .then(dbPostData => {res.json(dbPostData)})
+    .catch(err => res.json(err));
+});
+
+router.post('/', (req, res) => {
+  Post.create({
+    title: req.body.title,
+    content: req.body.content,
+    user_id: req.body.user_id
+  })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => res.status(500).json(err));
+});
+
+router.put('/:id', (req, res) => {
+  Post.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(dbPostData => res.status(500).json(dbPostData));
+});
+
+router.delete('/:id', (req, res) => {
+  Post.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(dbPostData => res.status(500).json(dbPostData));
+})
+
+module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,53 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
+const { Post, User } = require('../models');
 
 router.get('/', (req, res) => {
-  res.render('homepage', { loggedIn: req.session.loggedIn });
+  Post.findAll({
+    attributes: [
+      'id',
+      'title',
+      'content',
+      'user_id',
+      'created_at',
+    ],
+    include: {
+      model: User,
+      attributes: ['username']
+    }
+  })
+    .then(dbPostData => {
+      //serialize data
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+    })
+    .catch(err => res.json(err));
 });
+
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'title',
+      'content',
+      'user_id',
+      'created_at',
+    ],
+    include: {
+      model: User,
+      attributes: ['username']
+    }
+  })
+    .then(dbPostData => {
+      //serialize data
+      const post = dbPostData.get({ plain: true });
+      res.render('single-post', { post, loggedIn: req.session.loggedIn });
+    })
+    .catch(err => res.status(500).json(err));
+})
 
 router.get('/login', (req, res) => {
   res.render('login');
